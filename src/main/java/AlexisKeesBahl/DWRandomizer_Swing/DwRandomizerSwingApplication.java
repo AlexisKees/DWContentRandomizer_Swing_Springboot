cf4c0e573e30095520ff2c577465edcfc0c9fc76
Alexis Kees Bahl <alexiskeesbahl@gmail.com>
2025-11-07 09:58:41 -0300

    initial commit

---
+++ src/main/java/AlexisKeesBahl/DWRandomizer_Swing/DwRandomizerSwingApplication.java
@@ -0,0 +1,13 @@
+package AlexisKeesBahl.DWRandomizer_Swing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwRandomizerSwingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwRandomizerSwingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AlexisKeesBahl/DWRandomizer_Swing/DwRandomizerSwingApplicationTests.java
@@ -0,0 +1,13 @@
+package AlexisKeesBahl.DWRandomizer_Swing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwRandomizerSwingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
